ebf17a2117f8867f6b3fa55a3013039a061170cd
Matthew Stagner <matthew.stagner@gmail.com>
2018-12-27 11:52:30 -0600

    Quickstart

--- quickstart.py
+++ quickstart.py
@@ -6,8 +6,8 @@ from instapy.util import smart_run
 
 
 # login credentials
-insta_username = ''
-insta_password = ''
+insta_username = 'mcstagner'
+insta_password = 'DuckHunting2019$$**'
 
 # get an InstaPy session!
 # set headless_browser=True to run InstaPy in the background
@@ -21,16 +21,16 @@ with smart_run(session):
     # general settings
     session.set_relationship_bounds(enabled=True,
                                       delimit_by_numbers=True,
-                                       max_followers=4590,
-                                        min_followers=45,
-                                        min_following=77)
+                                       max_followers=7000,
+                                        min_followers=700,
+                                        min_following=1000)
     
-    session.set_dont_include(["friend1", "friend2", "friend3"])
-    session.set_dont_like(["pizza", "#store"])
+    # session.set_dont_include(["friend1", "friend2", "friend3"])
+    session.set_dont_like(['marykay', 'vintagefashion'])
     
     
     # activity
-    session.like_by_tags(["natgeo"], amount=10)
+    session.like_by_tags(['waterfowlphotography', 'duckhunting', 'sitkagear', 'drakewaterfowl', 'wildlifephotographer', 'wildlifephotography'], amount=50)
 
 
 
